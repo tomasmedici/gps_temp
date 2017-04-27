db599d9c02c41e52076245d7a533dc72aa1b9e26
tomasmedici <tomas@medici.dk>
2017-04-27 13:51:17 +0200

    second new commit - developer

--- app/src/main/java/com/example/gps_temp/DatabaseHandler.java
+++ app/src/main/java/com/example/gps_temp/DatabaseHandler.java
@@ -17,9 +17,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME   = "kopholderManager";
     private static final String TABLE_KOPHOLDER = "kopholder";
-    private static final String KEY_BRAND = "brand";_
-    private static final String KEY_DIAMETER = "diameter";_
-    private static final String KEY_COLOUR = "colour";_
+    private static final String KEY_BRAND = "brand";
+    private static final String KEY_DIAMETER = "diameter";
+    private static final String KEY_COLOUR = "colour";
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -55,7 +55,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
     //getKopholder()
     //getting single kopholder
-    public Kopholder getKopholder(String brand) {
+
+    public Kopholder getKopholder(int brand) {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(TABLE_KOPHOLDER, new String[] { KEY_BRAND,
@@ -64,11 +65,12 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         if (cursor != null)
             cursor.moveToFirst();
 
-        Kopholder kopholder = new Kopholder(Integer.parseInt(cursor.getString(0)),
-                cursor.getString(1), cursor.getString(2));
-        // return contact
+        Kopholder kopholder = new Kopholder(cursor.getString(0),
+                Integer.parseInt(cursor.getString(1)), cursor.getString(2));
+        // return kopholder
         return kopholder;
     }
+
     //getAllKopholder
     //getting all kopholder
     public List<Kopholder> getAllKopholder() {
@@ -83,8 +85,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         if (cursor.moveToFirst()) {
             do {
                 Kopholder kopholder = new Kopholder();
-                kopholder.setBrand(Integer.parseInt(cursor.getString(0)));
-                kopholder.setDiameter(cursor.getString(1));
+                kopholder.setBrand(cursor.getString(0));
+                kopholder.setDiameter(Integer.parseInt(cursor.getString(1)));
                 kopholder.setColour(cursor.getString(2));
                 // Adding contact to list
                 kopholderList.add(kopholder);
@@ -96,14 +98,34 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
     //Getting Kopholder count
     public int getKopholderCount() {
+        String countQuery = "SELECT * FROM " + TABLE_KOPHOLDER;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
 
+        // Return count
+        return cursor.getCount();
     }
     //Updating single Kopholder
     public int updateKopholder(Kopholder kopholder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_BRAND, kopholder.getBrand());
+        values.put(KEY_DIAMETER, kopholder.getDiameter());
+        values.put(KEY_COLOUR, kopholder.getColour());
+
+        // updating row
+        return db.update(TABLE_KOPHOLDER, values, KEY_BRAND + " = ?",
+                new String[] { String.valueOf(kopholder.getBrand()) });
 
     }
     //Deleting single Kopholder
     public void deleteKopholder(Kopholder kopholder) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_KOPHOLDER, KEY_BRAND + " = ?",
+                new String[] {String.valueOf(kopholder.getBrand()) });
+        db.close();
 
     }
 }
